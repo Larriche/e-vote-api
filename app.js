7f1f94f32d9ad3ab6e365bcd467c2c094dc8c070
Richman Larry Clifford <larriche99@gmail.com>
2018-07-11 00:08:19 +0000

    Refactored db connection string

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
-const connStr = 'mongodb://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@localhost:27017/evote';
+const db_username = process.env.DB_USERNAME;
+const db_password = process.env.DB_PASSWORD;
+
+const connStr = `mongodb://${db_username}:${db_password}@localhost:27017/evote`;
 
 // Connect to MongoDB database
 mongoose.connect(connStr, { useNewUrlParser: true, auth: { authdb: "admin" } });
