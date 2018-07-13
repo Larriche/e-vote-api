5ed14acb30e897e53df727bde686ce282aaa7d94
Richman Larry Clifford <larriche99@gmail.com>
2018-07-13 11:11:58 +0000

    Added method for updating elections

--- src/controllers/elections.js
+++ src/controllers/elections.js
@@ -69,6 +69,73 @@ const elections = {
                 status: 'failed'
             });
         }
+    },
+
+    /**
+     * Update election whose id is given with given data
+     *
+     * @param {Object} request Http request
+     * @param {Object} response Http Response
+     * @param {Object} next The next callable
+     */
+    update (request, response, next) {
+        // Set up request data fields validator
+        let validator = new Validator(request.body, {
+            name: 'required',
+            start_time: 'required',
+            end_time: 'required'
+        });
+
+        if (validator.passes()) {
+            let electionData = {
+                name: request.body.name,
+                start_time: request.body.start_time,
+                end_time: request.body.end_time
+            };
+
+            const noObjectMessage = 'Election was not found';
+
+            Election.findByIdAndUpdate(request.params.id, electionData)
+                .then(updatedElection => {
+                    // Reload saved election with related items
+                    Election.findOne({ _id: request.params.id })
+                        .populate('user', 'id name email')
+                        .then(election => {
+                            if (!election) {
+                                return response.status(404).json({
+                                    status: 'sofailed',
+                                    message: noObjectMessage
+                                });
+                            }
+
+                            let data = {
+                                election,
+                                status: 'success'
+                            };
+
+                            return response.status(200).json(data);
+                        })
+                        .catch(error => {
+                            error.status = 500;
+                            next(error);
+                        });
+                }).catch(error => {
+                    if (error.kind === 'ObjectId') {
+                        return response.status(404).json({
+                            status: 'failed',
+                            message: noObjectMessage
+                        });
+                    }
+
+                    error.status = 500;
+                    next(error);
+                });
+        } else {
+            response.status(422).json({
+                errors: validator.errors.all(),
+                status: 'failed'
+            });
+        }
     }
 };
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -15,6 +15,7 @@ function routes(router) {
     // Election Routes
     router.get('/elections', elections.index);
     router.post('/elections', elections.store);
+    router.put('/elections/:id', elections.update);
 }
 
 const setupRouter = function (app) {
